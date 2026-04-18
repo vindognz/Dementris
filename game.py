fb0d62548e2441ccdd9c684c4d8ba985a1398a17
Vincent Rodley <vindognz@gmail.com>
2026-04-18 14:28:50 +1200

    pretty awesome sauce game class

---
+++ game.py
@@ -0,0 +1,94 @@
+import pygame
+
+WIDTH = 600
+HEIGHT = 800
+FPS = 60
+TITLE = "Dementris"
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption(TITLE)
+
+        self.clock = pygame.time.Clock()
+        self.load_assets()
+        self.running = True
+        self.playing = False
+        self.dt = 0
+
+    def new(self):
+        # reset run stuff here
+        self.playing = True
+
+    def load_assets(self):
+        # load all assets (sounds, images, etc.)
+        self.font = pygame.font.SysFont(None, 32)
+        # self.font = pygame.font.Font('path', size)
+
+    def run(self):
+        # main game loop
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000
+            self.events()
+            self.update()
+            self.draw()
+
+    def update(self):
+        # update game stuff
+        pass
+
+    def draw(self):
+        # draw everything
+        self.screen.fill((0, 0, 0))
+
+        pygame.display.flip()
+
+    def events(self):
+        # events and all that
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+
+
+    def wait_for_key_screen(self, title: pygame.Surface, subtitle: pygame.Surface):
+        waiting = True
+        title_rect = None
+        subtitle_rect = None
+
+        if title:
+            title_rect = title.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+        if subtitle:
+            subtitle_rect = subtitle.get_rect(center=(WIDTH // 2, HEIGHT // 2 + HEIGHT // 20))
+
+        while waiting and self.running:
+            self.dt = self.clock.tick(FPS) / 1000
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                    waiting = False
+                elif event.type == pygame.KEYDOWN:
+                    waiting = False
+            
+            self.screen.fill((50, 50, 50))
+            if title:
+                self.screen.blit(title, title_rect)
+            if subtitle:
+                self.screen.blit(subtitle, subtitle_rect)
+            pygame.display.flip()
+
+
+    def show_start_screen(self):
+        # title screen
+        prompt = self.font.render("Press any key to start!", True, (255, 255, 255))
+        self.wait_for_key_screen(prompt, None)
+
+    def show_go_screen(self):
+        # game over screen or restart or smth
+
+        pass
--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello, world!")
+from game import Game
+
+
+def main():
+    game = Game()
+    game.show_start_screen()
+
+    while game.running:
+        game.new()
+        game.run()
+        game.show_go_screen()
+
+
+if __name__ == "__main__":
+    main()
